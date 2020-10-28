af8753d770280e8417ba34574bdd366a0f585ad0
deedeedextor <zavinagi_15@abv.bg>
2020-10-28 11:13:27 +0200

    Swapping numbers and strings

---
+++ SwappingTwoNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SwappingTwoNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SwappingTwoNumbersWithTemp(10, 20, 0);
+            Console.WriteLine();
+
+            SwappingTwoNumbersWithAddition(10, 20);
+            Console.WriteLine();
+
+            SwappingTwoNumbersWithMultiplication(10, 20);
+            Console.WriteLine();
+        }
+
+        private static void SwappingTwoNumbersWithMultiplication(int x, int y)
+        {
+            Console.WriteLine($"Before swapping: {x}, {y}");
+
+            x = x * y;
+            y = x / y;
+            x = x / y;
+
+            Console.WriteLine($"After swapping: {x}, {y}");
+        }
+
+        private static void SwappingTwoNumbersWithAddition(int x, int y)
+        {
+            Console.WriteLine($"Before swapping: {x}, {y}");
+
+            x = x + y;
+            y = x - y;
+            x = x - y;
+
+            Console.WriteLine($"After swapping: {x}, {y}");
+        }
+
+        private static void SwappingTwoNumbersWithTemp(int x, int y, int z)
+        {
+            Console.WriteLine($"Before swapping: {x}, {y}");
+
+            z = x;
+            x = y;
+            y = z;
+
+            Console.WriteLine($"After swapping: {x}, {y}");
+        }
+    }
+}
---
+++ SwappingTwoStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwappingTwoStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SwappingTwoStringsWithoutThirdOne("str", "rts");
+            Console.WriteLine();
+        }
+
+        private static void SwappingTwoStringsWithoutThirdOne(string x, string y)
+        {
+            Console.WriteLine($"Before swapping: {x}, {y}");
+
+            x = x + y;
+            y = x.Substring(0, x.Length - y.Length);
+            x = x.Substring(y.Length);
+
+            Console.WriteLine($"After swapping: {x}, {y}");
+        }
+    }
+}
