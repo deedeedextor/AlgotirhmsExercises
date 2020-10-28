c9b6d315e57024276a394d06a43d5f5a39514fef
deedeedextor <zavinagi_15@abv.bg>
2020-10-28 11:14:51 +0200

    Check for prime numbers

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace PrimeNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrimeNumber(103));
+            PrimeNumbersBetweenAGivenRange(1, 20);
+
+        }
+
+        private static void PrimeNumbersBetweenAGivenRange(int x, int y)
+        {
+            for (int i = x; i <= y; i++)
+            {
+                int counter = 0;
+
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if (counter == 0 && i != 1)
+                {
+                    Console.Write($"{i } ");
+                }
+            }
+        }
+
+        private static bool IsPrimeNumber(int x)
+        {
+            bool isPrime = true;
+
+            for (int i = 2; i < x/2; i++)
+            {
+                if (x % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
