b560c89a6690d4862a343d000da47da91447ec73
deedeedextor <zavinagi_15@abv.bg>
2020-10-29 13:55:04 +0200

    Find Palindrome and Armstrong numbers

---
+++ ArmstrongNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ArmstrongNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsArmstrongNumber(8208));
+            FindArmstrongNumbersBetweenGivenRange(153, 500);
+        }
+
+        private static void FindArmstrongNumbersBetweenGivenRange(int x, int y)
+        {
+            for (int i = x; i < y; i++)
+            {
+                int sum = 0;
+                int temporaryNumber = i;
+                int temp = 0;
+                int length = i.ToString().Length;
+
+                while (temporaryNumber != 0)
+                {
+                    temp = temporaryNumber % 10;
+                    temporaryNumber = temporaryNumber / 10;
+                    sum += (int)Math.Pow(temp, length);
+                }
+
+                if (sum == i)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        private static bool IsArmstrongNumber(int x)
+        {
+            bool isArmstrong = false;
+            int i = 0;
+            int digitCount = 0;
+            int[] digitArray = new int[10];
+            double sum = 0;
+
+            int temporaryNumber = x;
+
+            while (x > 0)
+            {
+                digitArray[i++] = x % 10;
+                x = x / 10;
+                digitCount++;
+            }
+
+            for (i = 0; i < digitCount; i++)
+            {
+                sum += Math.Pow(digitArray[i], digitCount);
+            }
+
+            if (sum == temporaryNumber)
+            {
+                isArmstrong = true;
+            }
+
+            return isArmstrong;
+        }
+    }
+}
---
+++ PalindromeNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeNumbersAndStrings
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsAPalindromeNumber(121));
+            Console.WriteLine(IsAPalindromeString("abcba"));
+        }
+
+        private static bool IsAPalindromeString(string x)
+        {
+            bool isPalindrome = false;
+
+            // options with foreach or for loop
+            char[] xArray = x.ToCharArray();
+            Array.Reverse(xArray);
+            string reverse = new string(xArray);
+
+            if (x == reverse)
+            {
+                isPalindrome = true;
+            }
+
+            return isPalindrome;
+        }
+
+        private static bool IsAPalindromeNumber(int x)
+        {
+            int reminder = 0, sum = 0;
+            bool isPalindrome = false;
+            int temp = x;
+
+            // with the same approach we cam reverse a number
+            do
+            {
+                reminder = x % 10;
+
+                sum = (sum * 10) + reminder;
+
+                x = x / 10;
+
+            } while (x > 0);
+
+            if (temp == sum)
+            {
+                isPalindrome = true;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
