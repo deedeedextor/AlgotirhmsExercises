4490a005f6b1d4a48735b50d83a15abaabfa8afb
deedeedextor <zavinagi_15@abv.bg>
2020-10-30 14:45:04 +0200

    Exercise Brute Force algorithm

---
+++ BruteForceAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static BruteForceAlgorithm.BruteForceAlgo;
+
+namespace BruteForceAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BruteForceTest testCallBack = delegate (ref char[] testChars)
+            {
+                var str = new string(testChars);
+                return str == "bbc";
+            };
+
+            bool result = BruteForce("abcde", 1, 5, testCallBack);
+            Console.WriteLine(result);
+        }
+    }
+}
