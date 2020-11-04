79692f3f556f638d52de4bef51de3a978b483a65
deedeedextor <zavinagi_15@abv.bg>
2020-11-04 16:32:54 +0200

    Reverse each word in a string

---
+++ ReverseEachWordInAString/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseEachWordInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseEachWordInAStringUsingStack("Hello and welcome");
+            ReverseEachWordInAStringUsingLinq("Hello and welcome");
+        }
+
+        private static void ReverseEachWordInAStringUsingLinq(string str)
+        {
+            string reversedWordString = string.Join(" ", str.Split(' ').Select(x => new string(x.Reverse().ToArray())));
+
+            Console.WriteLine(reversedWordString);
+        }
+
+        private static void ReverseEachWordInAStringUsingStack(string str)
+        {
+            Stack<char> charStack = new Stack<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    charStack.Push(str[i]);
+                }
+                else
+                {
+                    while (charStack.Count > 0)
+                    {
+                        Console.Write(charStack.Pop());
+                    }
+                    Console.Write(" ");
+                }
+            }
+
+            while (charStack.Count > 0)
+            {
+                Console.Write(charStack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
