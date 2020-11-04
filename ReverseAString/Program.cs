78b559d89274ec72048e48cc6e4dbd6bfe9db8b1
deedeedextor <zavinagi_15@abv.bg>
2020-11-04 16:32:29 +0200

    Reverse a string

---
+++ ReverseAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseAStringUsingBuildinMethod("Welcome to my world");
+            ReverseAStringUsingForLoop("Welcome to my world");
+
+        }
+
+        private static void ReverseAStringUsingForLoop(string str)
+        {
+            string reversedString = string.Empty;
+
+            for (int i = str.Length - 1; i >=0 ; i--)
+            {
+                reversedString += str[i];
+            }
+
+            Console.WriteLine(reversedString);
+        }
+
+        private static void ReverseAStringUsingBuildinMethod(string str)
+        {
+            char[] charString = str.ToCharArray();
+
+            Array.Reverse(charString);
+
+            Console.WriteLine(new string(charString));
+        }
+    }
+}
