0e548e018d8c62f972c9a17edc559d1dc9201c8e
deedeedextor <zavinagi_15@abv.bg>
2020-11-11 13:25:20 +0200

    Add left and right circular rotation of an array

---
+++ LeftCircularRotationOfArray/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace RightLeftCircularRotationOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformRightCircularRotationInAnArray(new int[6] { 1, 2, 3, 4, 5, 6 });
+            PerformLeftCircularRotationInAnArray(new int[6] { 1, 2, 3, 4, 5, 6 });
+        }
+
+        private static void PerformLeftCircularRotationInAnArray(int[] array)
+        {
+            int temp;
+
+            for (int i = array.Length - 1; i > 0 ; i--)
+            {
+                temp = array[array.Length - 1];
+                array[array.Length - 1] = array[i - 1];
+                array[i - 1] = temp;
+            }
+
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void PerformRightCircularRotationInAnArray(int[] array)
+        {
+            int temp;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                temp = array[0];
+                array[0] = array[i + 1];
+                array[i + 1] = temp;
+            }
+
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
