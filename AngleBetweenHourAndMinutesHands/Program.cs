25e83a85a7419a3b06d9b1b012805a8c923d3b79
deedeedextor <zavinagi_15@abv.bg>
2020-11-11 16:21:59 +0200

    Find the angle between the hours and minutes hands

---
+++ AngleBetweenHourAndMinutesHands/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AngleBetweenHourAndMinutesHands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindTheAngleBetweenHourAndMinutesHands(9, 30);
+        }
+
+        private static void FindTheAngleBetweenHourAndMinutesHands(int hours, int minutes)
+        {
+            // Each hour represent an angle of 30 degrees (360 / 12)
+            double hourInDegrees = (hours * 30) + (minutes * 30.0 / 60);
+
+            // Each minute represents an angle of 6 degrees (360 / 60)
+            double minuteInDegrees = minutes * 6;
+
+            double diff = Math.Abs(hourInDegrees - minuteInDegrees);
+
+            if (diff > 180)
+            {
+                diff = 360 - diff;
+            }
+
+            Console.WriteLine($"The angle between {hours} hour and {minutes} minute is {diff} degrees");
+        }
+    }
+}
