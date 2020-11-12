2a95351478d1b29b1b40de13303949cb159d9e09
deedeedextor <zavinagi_15@abv.bg>
2020-11-12 15:12:23 +0200

    Find if two strings are anagrams

---
+++ Anagrams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindFindIfTwoStringsAreAnagrams("silent", "Listen");
+            FindFindIfTwoStringsAreAnagrams(" ", "   ");
+        }
+
+        private static void FindFindIfTwoStringsAreAnagrams(string wordOne, string wordTwo)
+        {
+            if (wordOne.Length > 0 && wordTwo.Length > 0 && !string.IsNullOrWhiteSpace(wordOne) && !string.IsNullOrWhiteSpace(wordTwo))
+            {
+                char[] wordOneChar = wordOne.ToLower().ToCharArray();
+                char[] wordTwoChar = wordTwo.ToLower().ToCharArray();
+
+                Array.Sort(wordOneChar);
+                Array.Sort(wordTwoChar);
+
+                string newWordOne = new string(wordOneChar);
+                string newWordTwo = new string(wordTwoChar);
+
+                if (newWordOne == newWordTwo)
+                {
+                    Console.WriteLine("Yes! Words \"{0}\" and \"{1}\" are Anagrams", wordOne, wordTwo);
+                }
+                else
+                {
+                    Console.WriteLine("No! Words \"{0}\" and \"{1}\" are not Anagrams", wordOne, wordTwo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please, enter two words!");
+            }
+        }
+    }
+}
