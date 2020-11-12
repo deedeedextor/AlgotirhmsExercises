8fbe1a795aed5bf2b34ef9285dadd822562a8eaa
deedeedextor <zavinagi_15@abv.bg>
2020-11-12 15:41:12 +0200

    Find if a word is a palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("civic"));
+            Console.WriteLine(IsPalindrome("CSharp"));
+        }
+
+        private static bool IsPalindrome(string str)
+        {
+            int min = 0;
+            int max = str.Length - 1;
+
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+
+                char a = str[min];
+                char b = str[max];
+
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
