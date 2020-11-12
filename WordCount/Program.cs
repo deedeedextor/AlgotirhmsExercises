698befa71403da17a9bc328216ad8663986e7190
deedeedextor <zavinagi_15@abv.bg>
2020-11-12 15:25:56 +0200

    Count the words in a string

---
+++ WordCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountWordsInAString("Welcome to my world"));
+        }
+
+        private static int CountWordsInAString(string str)
+        {
+            int count = 0;
+
+            str = str.Replace("  ", " ").Trim();
+
+            if (str != "")
+            {
+                foreach (var word in str.Split(" "))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
