b012c7b00cda5784c8e3934b763b74a956c8a5ee
deedeedextor <zavinagi_15@abv.bg>
2020-11-13 15:40:28 +0200

    Merge two sorted arrays

---
+++ MergeSortedArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergeSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergeSortedArrays(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 });
+        }
+
+        private static void MergeSortedArrays(int[] arrOne, int[] arrTwo)
+        {
+            var sortedArray = new List<int>();
+
+            sortedArray.AddRange(arrOne);
+            sortedArray.AddRange(arrTwo);
+
+            Console.WriteLine(string.Join(", ", sortedArray));
+        }
+    }
+}
