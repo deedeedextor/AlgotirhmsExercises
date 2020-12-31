8a0b1e7f4d2839bd1714d207c591f856f6e0fbe2
deedeedextor <zavinagi_15@abv.bg>
2020-12-31 15:41:00 +0200

    Add exercise Return remainder

---
+++ ReturnRemainder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReturnRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnRemainder(3, 0));
+        }
+
+        private static int ReturnRemainder(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new Exception("Can not divide by zero");
+            }
+
+            if (x < y)
+            {
+                throw new Exception("The number being divided can not be less than the divisor");
+            }
+
+            return x % y;
+        }
+    }
+}
